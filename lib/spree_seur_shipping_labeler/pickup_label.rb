5777ba8a55d453b6b07aeeb32568201d6a2f3179
gonzalogarcia <gjaubert@gmail.com>
2017-11-16 08:49:53 +0000

    Changed by SEUR instructions from N to S the tests fields

--- lib/spree_seur_shipping_labeler/pickup_label.rb
+++ lib/spree_seur_shipping_labeler/pickup_label.rb
@@ -137,9 +137,9 @@ module SpreeSeurShippingLabeler
           codPostal_consignatario: customer_address[:zip_code]&.remove(' ') || '',
           email_consignatario: customer_address[:email]  || '',
           pais_consignatario: customer_address[:country_iso] || '',
-          test_email: 'N',
-          test_preaviso: 'N',
-          test_reparto: 'N',
+          test_email: 's',
+          test_preaviso: 's',
+          test_reparto: 's',
           telefono_consignatario: customer_address[:phone_number]  || '',
           cod_centro: '',
           codigo_pais_origen: seller_address[:country_iso]
